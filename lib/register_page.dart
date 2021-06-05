79d519d06b67af4733bab12cb229b1ac6e7d093a
kimhowon123 <kimhowon123@naver.com>
2021-06-06 03:35:13 +0900

    11

--- lib/register_page.dart
+++ lib/register_page.dart
@@ -22,6 +22,7 @@ class _RegisterPageState extends State<RegisterPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         title: Text(widget.title),
       ),
