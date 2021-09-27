45726f29bada2684d557bd8946e236765b11dd6e
kimhowon123 <kimhowon123@naver.com>
2021-09-27 20:29:51 +0900

    09272029

--- lib/Notice.dart
+++ lib/Notice.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-<<<<<<< HEAD
 
 class Notice extends StatelessWidget {
 @override
@@ -9,15 +8,6 @@ Widget build(BuildContext context) {
     appBar: AppBar(title: Text('공지사항')),
   );
 }
-=======
-import 'package:myapp/View.dart';
-
-class Notice extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-  
-  }
->>>>>>> 0633b1cfc9123498fb1fb986637866028723ec7c
 }
 
 
